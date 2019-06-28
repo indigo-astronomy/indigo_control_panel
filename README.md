1d67460a9ed14f9f33b24ee62496a114b9e525f9
Rumen G.Bogdanovski <rumen@skyarchive.org>
2019-06-28 18:03:47 +0300

    add license text to sources

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# control-panel
+# indigo_panel
 
 To build this code on Mac, it is necessary to create a symlink to the location of your indigo source tree:
 
@@ -7,4 +7,4 @@ ln -s <path-to-indigo> indigo
 To build on all OS, you will need a clone of the QtZeroConf project:
 
 git clone https://github.com/jbagg/QtZeroConf qtzeroconf
-
+git checkout 800e66e4f3160f75f163e500a211eab9cc4816c9
--- browserwindow.cpp
+++ browserwindow.cpp
@@ -1,3 +1,22 @@
+// Copyright (c) 2019 Rumen G.Bogdanovski & David Hulse
+// All rights reserved.
+//
+// You can use this software under the terms of 'INDIGO Astronomy
+// open-source license' (see LICENSE.md).
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QSplitter>
@@ -414,8 +433,9 @@ void BrowserWindow::on_about_act() {
 		"<b>INDIGO Control Panel</b><br>"
 		"Version 0.1-b1</b><br><br>"
 		"Authors:<br>"
-		"David Hulse<br>"
-		"Rumen G.Bogdanovski<br><br>"
+		"Rumen G.Bogdanovski<br>"
+		"David Hulse<br><br>"
+		"You can use this software under the terms of <b>INDIGO Astronomy open-source license</b><br><br>"
 		"Copyright ©2019, The INDIGO Initiative.<br>"
 		"<a href='http://www.indigo-astronomy.org'>http://www.indigo-astronomy.org</a>"
 	);
