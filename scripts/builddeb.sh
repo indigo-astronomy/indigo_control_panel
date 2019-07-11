cbda78a69bbb9803769ec62c1e3e57d2d7e56a28
Rumen G.Bogdanovski <rumen@skyarchive.org>
2019-07-11 18:31:57 +0300

    deb build fixes from thomas

--- scripts/builddeb.sh
+++ scripts/builddeb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GIT_VERSION="0.1-2-poc"
+GIT_VERSION=""
 DEB_VERSION=""
 
 DEBFULLNAME="Rumen Bogdanovski"
@@ -11,32 +11,35 @@ __check_file_exits() {
 }
 
 # Check for files where version number shall be replaced.
-__check_file_exits "debian/changelog"
 __check_file_exits "version.h"
+
+# Make sure debian/changelog does not exists because we will genrate it.
+rm -f debian/changelog
 
 # Check for Debian package building executables and tools.
 [ ! $(which dch) ] && { echo "executable 'dch' not found install package: 'devscripts'"; exit 1; }
 [ ! $(which dpkg-buildpackage) ] && { echo "executable 'dpkg-buildpackage' not found install package: 'dpkg-dev'"; exit 1; }
 [ ! $(which cdbs-edit-patch) ] && { echo "executable 'cdbs' not found install package: 'cdbs'"; exit 1; }
 
-# Read DEB package version from debian/changelog
-DEB_VERSION=$(cat debian/changelog | grep -m 1 -Po '^indigo-control-panel.*\(\K[^)]+')
-[ ! ${DEB_VERSION} ] && { echo "Changelog version cannot be read from debian/changelog"; exit 1; }
-
 # Read GIT TAG version via command git describe
-# TODO.
-[ ! ${GIT_VERSION} ] && { echo "GIT tag version cannot be read"; exit 1; }
+GIT_VERSION=$(git describe --tags)
+[ $? -ne 0 ] && { echo "GIT tag version cannot be read"; exit 1; }
 
-# Update debian/changelog when GIT version does not match with debian/changelog version.
-if [ "${GIT_VERSION}" != "${DEB_VERSION}" ]; then
-    dch --newversion ${GIT_VERSION} --distribution unstable --nomultimaint -t "Build from official master upstream."
-else
-    echo "Debian changelog version and GIT tag version are equal, no changelog entry update required"
-    exit 1
-fi
+# If parameter is provided, try this tag to checkout and build package.
+[ "$#" -eq 1 ] && GIT_VERSION=${1}
+
+# Checkout the desired tag/version we want to build packages.
+git checkout ${GIT_VERSION} >/dev/null 2>&1
+[ $? -ne 0 ] && { echo "version '${1}' does not exists in git"; exit 1; }
+
+# Create entry in debian/changelog.
+dch --create --package "indigo-control-panel" --newversion ${GIT_VERSION} --distribution unstable --nomultimaint -t "Build from official upstream."
 
 # Update version.h.
 sed -i "s/\(PANEL_VERSION \).*/\1\"${GIT_VERSION}\"/g" version.h
 
 # Finally build the package.
 dpkg-buildpackage \-us \-uc \-I.git \-I\*.out[0-9]\* \-I\*.swp
+
+# Cleanup debian/changelog.
+rm -f debian/changelog
