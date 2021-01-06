25931880de6f69c5028068c01e6a6d0983046aec
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-01-06 17:48:18 +0200

    automate windows installer build

---
+++ windows/build_windows.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+APP=indigo_control_panel
+
+SAVE_PATH=$PATH:/c/Program\ Files\ \(x86\)/Inno\ Setup\ 6/
+
+export PATH=/c/Qt/5.12.10/mingw73_32/bin:/c/Qt/Tools/mingw730_32/bin/:$SAVE_PATH
+
+pushd .
+[ ! -d "${APP}_32" ] && mkdir ${APP}_32
+cd ${APP}_32
+qmake ../../${APP}.pro
+mingw32-make -f Makefile.release
+
+[ ! -d "${APP}" ] && mkdir ${APP}
+cd ${APP}
+cp ../release/${APP}.exe .
+windeployqt ${APP}.exe
+popd
+
+pushd .
+export PATH=/c/Qt/5.12.10/mingw73_64/bin:/c/Qt/Tools/mingw730_64/bin/:$SAVE_PATH
+[ ! -d "${APP}_64" ] && mkdir ${APP}_64
+cd ${APP}_64
+qmake ../../${APP}.pro
+mingw32-make -f Makefile.release
+
+[ ! -d "${APP}" ] && mkdir ${APP}
+cd ${APP}
+cp ../release/${APP}.exe .
+windeployqt ${APP}.exe
+popd
+
+iscc -DArch=32 ${APP}.iss
+iscc -DArch=64 ${APP}.iss
