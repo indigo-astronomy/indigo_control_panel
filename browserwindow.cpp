13c98895620989f3b8ff321359cc9dae23e8248a
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-02-03 12:06:01 +0200

    remove useless fix that is crashing

--- browserwindow.cpp
+++ browserwindow.cpp
@@ -329,10 +329,6 @@ void BrowserWindow::on_window_log(indigo_property* property, char *message) {
 
 	if (!message) return;
 
-	char *str = message;
-	while(*str && *str != '\r' && *str != '\n') str++;
-	*str = 0;
-
 	gettimeofday(&tmnow, NULL);
 #if defined(INDIGO_WINDOWS)
 	struct tm *lt;
