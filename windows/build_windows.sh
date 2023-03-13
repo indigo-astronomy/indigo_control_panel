ab3c43f3b4643a17b6f2235087aa2389f52a8bb2
Rumen G.Bogdanovski <rumen@skyarchive.org>
2023-03-13 12:23:22 +0200

    fix windows build using libindigo_client.dll

--- windows/build_windows.sh
+++ windows/build_windows.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 APP=indigo_control_panel
+QT_VER=5.15.2
+MINGW_VER=81
 
 SAVE_PATH=$PATH:/c/Program\ Files\ \(x86\)/Inno\ Setup\ 6/
 
-export PATH=/c/Qt/5.12.10/mingw73_32/bin:/c/Qt/Tools/mingw730_32/bin/:$SAVE_PATH
+export PATH=/c/Qt/${QT_VER}/mingw${MINGW_VER}_32/bin:/c/Qt/Tools/mingw${MINGW_VER}0_32/bin/:$SAVE_PATH
+
+pushd .
+cd ../external/indigo_sdk
+rm -r lib/
+mkdir lib/
+cp -r lib86/* lib/
+popd
 
 pushd .
 [ ! -d "${APP}_32" ] && mkdir ${APP}_32
@@ -15,11 +24,20 @@ mingw32-make -f Makefile.release
 [ ! -d "${APP}" ] && mkdir ${APP}
 cd ${APP}
 cp ../release/${APP}.exe .
+cp ../../../external/indigo_sdk/lib/libindigo_client.dll .
 windeployqt ${APP}.exe
 popd
 
+export PATH=/c/Qt/${QT_VER}/mingw${MINGW_VER}_64/bin:/c/Qt/Tools/mingw${MINGW_VER}0_64/bin/:$SAVE_PATH
+
 pushd .
-export PATH=/c/Qt/5.12.10/mingw73_64/bin:/c/Qt/Tools/mingw730_64/bin/:$SAVE_PATH
+cd ../external/indigo_sdk
+rm -r lib/
+mkdir lib/
+cp -r lib64/* lib/
+popd
+
+pushd .
 [ ! -d "${APP}_64" ] && mkdir ${APP}_64
 cd ${APP}_64
 qmake ../../${APP}.pro
@@ -28,6 +46,7 @@ mingw32-make -f Makefile.release
 [ ! -d "${APP}" ] && mkdir ${APP}
 cd ${APP}
 cp ../release/${APP}.exe .
+cp ../../../external/indigo_sdk/lib/libindigo_client.dll .
 windeployqt ${APP}.exe
 popd
 
