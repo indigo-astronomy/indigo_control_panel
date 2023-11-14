78ac7504cba8631b88616a5c8f6a31b887402e78
Rumen G.Bogdanovski <rumenastro@gmail.com>
2023-11-15 01:01:22 +0200

    change color balance to auto

--- blobpreview.cpp
+++ blobpreview.cpp
@@ -18,7 +18,6 @@
 
 #include <math.h>
 #include <fits/fits.h>
-#include <pixelformat.h>
 #include "blobpreview.h"
 #include <QPainter>
 
@@ -77,7 +76,7 @@ bool blob_preview_cache::create(indigo_property *property, indigo_item *item) {
 	pthread_mutex_lock(&preview_mutex);
 	QString key = create_key(property, item);
 	_remove(property, item);
-	const stretch_config_t sc = {preview_stretch_level, COLOR_BALANCE_NONE };
+	const stretch_config_t sc = {preview_stretch_level, COLOR_BALANCE_AUTO };
 	QImage *preview = create_preview(property, item, sc);
 	indigo_debug("preview: %s(%s) == %p\n", __FUNCTION__, key.toUtf8().constData(), preview);
 	if (preview != nullptr) {
