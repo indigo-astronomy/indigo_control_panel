2b5bc9140c386ada9ff1e7009fc73f8c92e013e5
Rumen G.Bogdanovski <rumenastro@gmail.com>
2024-11-27 22:50:28 +0200

    better signal handling

--- main.cpp
+++ main.cpp
@@ -21,15 +21,49 @@
 #include <QDir>
 #include <QStandardPaths>
 #include <QTextStream>
-
+#include <QSocketNotifier>
+#include <signal.h>
+#ifdef INDIGO_WINDOWS
+#include <QTimer>
+#include <windows.h>
+#else
+#include <QSocketNotifier>
 #include <signal.h>
 #include <unistd.h>
+#endif
 
 #include "browserwindow.h"
 #include <conf.h>
 
 conf_t conf;
 char config_path[PATH_LEN];
+
+#ifndef INDIGO_WINDOWS
+int sigpipe_fd[2];
+int sigint_fd[2];
+
+void handle_sigint(int) {
+	char a = 1;
+	write(sigint_fd[1], &a, sizeof(a));
+}
+
+void handle_sigpipe(int) {
+	char a = 1;
+	write(sigpipe_fd[1], &a, sizeof(a));
+}
+#else
+void write_conf();
+
+BOOL WINAPI console_handler(DWORD signal) {
+	if (signal == CTRL_C_EVENT) {
+		write_conf();
+		indigo_error("Configuration saved. Exiting...");
+		QTimer::singleShot(0, qApp, &QCoreApplication::quit);
+		return TRUE;
+	}
+	return FALSE;
+}
+#endif
 
 void write_conf() {
 	char filename[PATH_LEN];
@@ -111,7 +145,51 @@ int main(int argc, char *argv[]) {
 	BrowserWindow browser_window;
 	browser_window.show();
 
-	signal(SIGPIPE, SIG_IGN);
+#ifndef INDIGO_WINDOWS
+	// Handle SIGPIPE on Unix-like systems to prevent application crash
+	if (pipe(sigpipe_fd)) {
+		indigo_error("Couldn't create pipe for SIGPIPE");
+	}
+	struct sigaction sa_pipe;
+	sa_pipe.sa_handler = handle_sigpipe;
+	sigemptyset(&sa_pipe.sa_mask);
+	sa_pipe.sa_flags = 0;
+	sigaction(SIGPIPE, &sa_pipe, nullptr);
+
+	QSocketNotifier *sn_pipe = new QSocketNotifier(sigpipe_fd[0], QSocketNotifier::Read, &app);
+	QObject::connect(sn_pipe, &QSocketNotifier::activated, [&]() {
+		sn_pipe->setEnabled(false);
+		char tmp;
+		read(sigpipe_fd[0], &tmp, sizeof(tmp));
+		indigo_debug("SIGPIPE caught");
+		sn_pipe->setEnabled(true);
+	});
+
+	// Handle SIGINT (Ctrl+C) to save config and exit
+	if (pipe(sigint_fd)) {
+		indigo_error("Couldn't create pipe for SIGINT");
+	}
+	struct sigaction sa_int;
+	sa_int.sa_handler = handle_sigint;
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_flags = 0;
+	sigaction(SIGINT, &sa_int, nullptr);
+
+	QSocketNotifier *sn_int = new QSocketNotifier(sigint_fd[0], QSocketNotifier::Read, &app);
+	QObject::connect(sn_int, &QSocketNotifier::activated, [&]() {
+		sn_int->setEnabled(false);
+		char tmp;
+		read(sigint_fd[0], &tmp, sizeof(tmp));
+		write_conf();
+		indigo_debug("Configuration saved. Exiting...");
+		QCoreApplication::quit();
+		sn_int->setEnabled(true);
+	});
+#else
+	if (!SetConsoleCtrlHandler(console_handler, TRUE)) {
+		indigo_debug("Could not set control handler");
+	}
+#endif
 
 	return app.exec();
 }
