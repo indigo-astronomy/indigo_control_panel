c5e551d88f10522f133cdb239febfcc72ec086a9
Rumen G.Bogdanovski <rumenastro@gmail.com>
2026-05-16 23:50:51 +0300

    add forgotten files

---
+++ scripts/build_in_docker_local_indigo.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Copyright (c) 2019 Rumen G.Bogdanovski
+# All rights reserved.
+#
+# You can use this software under the terms of 'INDIGO Astronomy
+# open-source license'
+# (see https://github.com/indigo-astronomy/indigo/blob/master/LICENSE.md).
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS 'AS IS' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+baseImage="$1"
+# If image contains an architecture prefix like "amd64/debian:...", strip the prefix
+if echo "$baseImage" | grep -q "/"; then
+        baseImage="${baseImage#*/}"
+fi
+
+# Local INDIGO version to build against, e.g. "2.0-364" or "3.0-1". The flavor
+# (indigo / indigo3) is inferred from its major version, so no separate flavor
+# argument is needed. The local .deb is expected to be named
+# "<flavor>-<indigo_version>-<arch>.deb".
+indigoVersion="$4"
+if [ -z "$indigoVersion" ]; then
+        echo "Please specify the local INDIGO version"
+        exit 1
+fi
+case "$indigoVersion" in
+        3.*) flavor="indigo3" ;;
+        2.*) flavor="indigo" ;;
+        *)   echo "Cannot infer INDIGO flavor from version '$indigoVersion' (expected it to start with '2.' or '3.')"; exit 1 ;;
+esac
+if [ "$flavor" = "indigo3" ]; then
+        debName="indigo-control-panel-indigo3"
+else
+        debName="indigo-control-panel"
+fi
+
+# Map short arch names to docker platforms
+case "$3" in
+        amd64) platform="linux/amd64" ;;
+        i386)  platform="linux/386" ;;
+        armhf) platform="linux/arm/v7" ;;
+        arm64) platform="linux/arm64" ;;
+        *)     platform="" ;;
+esac
+
+echo FROM $baseImage >Dockerfile
+cat >>Dockerfile <<EOF
+LABEL maintainer="rumenastro@gmail.com"
+RUN apt-get -y update && apt-get -y install wget unzip build-essential autoconf autotools-dev libtool cmake libudev-dev libavahi-compat-libdnssd-dev libusb-1.0-0-dev libcurl4-gnutls-dev libz-dev git curl bsdmainutils qtbase5-dev qtmultimedia5-dev devscripts cdbs apt-transport-https
+RUN apt-get -y remove systemd; exit 0
+RUN echo 'deb [trusted=yes] https://indigo-astronomy.github.io/indigo_ppa/ppa indigo main' >>/etc/apt/sources.list
+RUN apt-get update
+COPY $flavor-$indigoVersion-$3.deb .
+RUN apt-get -y install ./$flavor-$indigoVersion-$3.deb
+ENV ICP_INDIGO_FLAVOR=$flavor
+COPY indigo-control-panel-$2.tar.gz .
+RUN tar -zxf indigo-control-panel-$2.tar.gz
+RUN rm indigo-control-panel-$2.tar.gz
+WORKDIR indigo-control-panel-$2
+RUN qmake
+RUN scripts/builddeb.sh $2 $flavor
+EOF
+if [ -n "$platform" ]; then
+        docker build --platform="$platform" -t icp .
+else
+        docker build -t icp .
+fi
+docker create --name icp icp
+docker cp icp:/${debName}_$2_$3.deb .
+docker container rm icp
+docker image rm icp
+rm Dockerfile
